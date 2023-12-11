dd7249c5ad5e159783cfaa9d28ab41227c349025
Suraj Rajendra Pardeshi <152958796+SurajPardeshi636@users.noreply.github.com>
2023-12-11 14:53:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("The MSG is " + i);
+            }
+
+            for (int j = 10; j >= 1; --j)
+            {
+                Console.WriteLine("hello " + j);
+            }
+            for(; ; )
+            {
+                Console.WriteLine("contiue"); // this is contionue and infinity foe loop
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
